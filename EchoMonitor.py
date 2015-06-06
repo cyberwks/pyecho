c461587dc0e9b498f73a239b1c8447bb519a7b77
Steve <steve@cyberwks.com>
2015-06-06 16:35:48 -0500

    Added API list, check all APIs
    Downloaded API list by hand, use it to introspect all Echo APIs

--- EchoMonitor.py
+++ EchoMonitor.py
@@ -3,12 +3,19 @@
 
 import PyEcho, getpass
 
+with open('api.txt', 'r') as f:
+    apis = f.readlines()
+
+# for api in apis :
+#     print "* " + api.strip() + "!"
+
 email = raw_input("Email: ")
 password = getpass.getpass()
 echo = PyEcho.PyEcho(email, password)
 
 if echo:
    print "API Monitor. Type an endpoint (starting with /api/) to fetch."
-   while True:
-      endpoint = raw_input("> ")
-      print echo.get(endpoint).text
+   for api in apis:
+       api = api.strip()
+       print "Using API : " + api
+       print echo.get(api).text
